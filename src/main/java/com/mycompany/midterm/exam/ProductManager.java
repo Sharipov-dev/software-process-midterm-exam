debf88aca5dd1e4956f9c75cb193f8193c3133a4
Arthur Sharipov <sharipov@sheridancollege.ca>
2026-02-18 14:01:30 -0500

    Added discount feature by Artur Sharipov

--- src/main/java/com/mycompany/midterm/exam/ProductManager.java
+++ src/main/java/com/mycompany/midterm/exam/ProductManager.java
@@ -9,6 +9,10 @@ package com.mycompany.midterm.exam;
  * @author sharipov
  */
 public class ProductManager {
+    
+    public double calculateDiscount(double price, double percent) {
+        return price - (price * percent / 100.0);
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");

478269e204410c72c3d3680806c7acf2c278c3e0
Arthur Sharipov <sharipov@sheridancollege.ca>
2026-02-18 13:55:02 -0500

    Initial Commit by Artur Sharipov

---
+++ src/main/java/com/mycompany/midterm/exam/ProductManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.midterm.exam;
+
+/**
+ *
+ * @author sharipov
+ */
+public class ProductManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
